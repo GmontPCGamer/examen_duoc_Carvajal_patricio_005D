e2e2331477acb94e9c74a1f3854d92a4ce1ddef5
Patricio Carvajal <p.carvajal@duocuc.cl>
2024-07-11 12:17:34 -0400

    opc3completa

--- fun.py
+++ fun.py
@@ -1,10 +1,10 @@
-import random, os, msvcrt
+import random, os, msvcrt, csv
 # ** elevado
 #aleatorio = random.randint(menor, mayor)
 trabajadores = ["juan pérez", "María García","Carlos López","Ana Martínez","Pedro Rodríguez","Laura Hernández","Miguel"
                 "Sánchez","Isabel Gómez","Francisco Díaz","Elena Fernández"]
 sueldos_t = []
-
+total_s=[]
 def menu():
     print("Menu")
     print("1) Generar Sueldos aleatorios")
@@ -26,7 +26,10 @@ def menu():
 def generar_s():
     for x in range(len(trabajadores)):
         sueldo_aleatorio = random.randint(300000, 2500000)
-        sueldo_t = trabajadores[x], sueldo_aleatorio
+        afp = sueldo_aleatorio * 0.12
+        salud = sueldo_aleatorio * 0.07
+        liquido = sueldo_aleatorio-afp-salud
+        sueldo_t = trabajadores[x], sueldo_aleatorio, salud, afp, liquido
         sueldos_t.append(sueldo_t)
 
 def clasificar_s():
@@ -37,7 +40,7 @@ def clasificar_s():
     print("Nombre Apellido| Sueldo")
     for x in range(len(sueldos_t)):
         if sueldos_t[x][1] > 0 and sueldos_t[x][1] < 800000:
-            print(sueldos_t[x])
+            print(sueldos_t[x][0], sueldos_t[x][1])
             total = total+1
     print("==============================")
     print("sueldos de 0 a 800.000: ",total)
@@ -49,7 +52,7 @@ def clasificar_s():
     print("Nombre Apellido| Sueldo")
     for x in range(len(sueldos_t)):
         if sueldos_t[x][1] > 800000 and sueldos_t[x][1] < 2000000:
-            print(sueldos_t[x])
+            print(sueldos_t[x][0], sueldos_t[x][1])
             total = total+1
     print("==============================")
     print("sueldos de 800.000 y 2000.000: ",total)
@@ -61,23 +64,56 @@ def clasificar_s():
     print("Nombre Apellido| Sueldo")
     for x in range(len(sueldos_t)):
         if sueldos_t[x][1] > 2000000:
-            print(sueldos_t[x])
+            print(sueldos_t[x][0], sueldos_t[x][1])
             total = total+1
     print("==============================")
     print("sueldos de mas de 2000.000: ",total)
     print("==============================")
+
+    resultado = 0
+    for x in range(len(sueldos_t)):
+        total = sueldos_t[x][1]
+        resultado = resultado+total
+    print(f"TOTAL SUELDOS:{resultado} ")
+
 def ver_estadisticas():
-    pass
+    resultado = 0
+    for x in range(len(sueldos_t)):
+        total = sueldos_t[x][1]
+        resultado = resultado+total
+    promedio = resultado/10
+    sueldos_t.sort()
+    print("el sueldo mas bajo es: ",sueldos_t[0][1])
+    print("el sueldo mas alto es: ", sueldos_t[9][1])
+    print(f"Promedio de sueldo es: {promedio}")
+
+    for x in range(len(sueldos_t)):
+        total = sueldos_t[x][1]
+        resultado = total*total
 def reporte_s():
-    pass
+    print("Nombre empleado | Sueldo Base | Descuento Salud | Descuento AFP | Sueldo Líquido")
+    for x in range(len(sueldos_t)):
+        print(f"{x+1})", sueldos_t[x])
+        
+        with open("listado_trabajadores", "w", newline="")as archivo:
+            
+        
+            
 def salir_p():
+
     datos_user = input("ingrese su nombre: ")
     rut = int(input('Ingrese su rut sin puntos, sin guion: '))
     os.system('cls')
     print('Finalizando programa...')
     print(f'Desarrollado por,{datos_user}')
     print(f'Rut: {rut}')
-    print("\n >>PRESIONE TECLA PARA FINALIZAR<<")
-    msvcrt.getch()
+    esperar_t()
     exit()
 
+##############################################
+    
+def esperar_t():
+    print("\n >>PRESIONE TECLA PARA FINALIZAR<<")
+    msvcrt.getch()
+def limpiar_p():
+    os.system("cls")
